a330fd076a8ef8ce64e2af03a9b5fc79ab1165dd
fightinfg <97812936+fightinfg@users.noreply.github.com>
2022-09-26 18:21:38 +0800

    Add files via upload

---
+++ js/自己的图案/triangle1.js
@@ -0,0 +1,142 @@
+"use strict";
+
+var gl;
+var points;
+
+window.onload = function init() {
+    var canvas = document.getElementById("trisquare-canvas");
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("WebGL isn't available");
+    }
+	
+	var vertexData =[];
+	vertexData.push(0.0, 0.0);
+	vertexData.push(1.0, 0.0);
+	vertexData.push(3/4,0.68687);
+	vertexData.push(1/4,0.68687);
+	
+    var N = 360;
+    vertexData.push(0.0, 0.0);//4
+    var r = 0.73095*2/3;
+    
+    for (var i = 0; i <= N; i++) {//4+361
+    	var theta = i * 2 * Math.PI / N+20* 2 * Math.PI / N;
+    	var x = r * Math.sin(theta);
+    	var y = r * Math.cos(theta);
+    	vertexData.push(x, y);
+    }
+    
+	var N1 = 100;
+	vertexData.push(0.0+3/16, 0.0+1/8);
+	var r =0.03;
+	
+	for (var i = 0; i <= N1; i++) {
+		var theta = i * 2 * Math.PI / N1;
+		var x = r * Math.sin(theta)+3/16;
+		var y = r * Math.cos(theta)+1/8;
+		vertexData.push(x, y);
+	}
+	
+	/**
+	//折线
+	vertexData.push(0.2,0.55);
+	vertexData.push(0.3,0.6);
+	vertexData.push(0.25,0.5);
+	**/
+	
+	//脸
+    var tcolor = [
+        0.99216, 0.96078, 0.90196
+    ];
+
+    var tricolor = [];
+    for( var i=0; i<=N+2; i++ ) {
+        tricolor.push(tcolor[0], tcolor[1], tcolor[2]);
+    }
+	
+	//身体
+    var scolor = [
+        0.80392, 0.52157, 0.24706
+    ];
+	
+    var sqcolor = [];
+    for (var i = 0; i < 4; i++ ) {
+        sqcolor.push(scolor[0], scolor[1], scolor[2]);
+    }
+
+    var sqcolor = sqcolor.concat(tricolor);
+	
+	//眼睛
+	var scolor2 = [
+	    0.0, 0.0, 0.0
+	];
+	
+	var sqcolor2 = [];
+	for (var i = 0; i < N1+2; i++ ) {
+	    sqcolor2.push(scolor2[0], scolor2[1], scolor2[2]);
+	}
+	
+	var sqcolor = sqcolor.concat(sqcolor2);
+	
+	//折线
+	var scolor3 = [
+	    0.0, 0.0, 0.0
+	];
+	
+	var sqcolor3 = [];
+	for (var i = 0; i < 3; i++ ) {
+	    sqcolor2.push(scolor3[0], scolor3[1], scolor3[2]);
+	}
+	
+	var vcolors = sqcolor.concat(sqcolor3);
+	
+	
+	
+
+    // Configure WebGL
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    // Load shaders and initialize attribute buffers
+    var program = initShaders(gl, "vertex-shader", "fragment1-shader");
+    gl.useProgram(program);
+
+    // Load the data into the GPU
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+    // Associate external shader variables with data buffer
+    var vPosition1 = gl.getAttribLocation(program, "aPosition");
+    gl.vertexAttribPointer(vPosition1, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition1);
+    
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vcolors), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program, "aColor");
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    /*
+    var program2 = initShaders(gl, "xxx", "xx" );
+    gl.useProgram(program2);
+
+    var vBuffer2 = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer2);
+    gl.buffeData(gl.ARRAY_BUFFER, new Float32Array(vertices2), gl.STATIC_DRAW);
+    */
+    render();
+}
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+    gl.drawArrays(gl.TRIANGLE_FAN, 4,68+4); //360+3
+	gl.drawArrays(gl.TRIANGLE_FAN, 362+4+1,362+4+1); 
+	
+}
+
